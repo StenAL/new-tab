f8922b3ea445d8e505bd3221c9cb72f2e5270a6c
Sten Laane <laanesten@gmail.com>
2020-09-20 14:14:59 +0100

    Update data fetching logic

--- src/components/BalanceContainer.js
+++ src/components/BalanceContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import 'currency-flags/dist/currency-flags.css'
 import CurrencyBalance from "./CurrencyBalance";
 import Modal from 'react-modal';
@@ -12,22 +12,16 @@ export default function BalanceContainer() {
 
     const [balance, setBalance] = React.useState({balances: []});
     const [modalOpen, setModalOpen] = React.useState(false);
-    const [displayedBalances, setDisplayedBalances] = React.useState(undefined);
-
-    if (!displayedBalances) {
-        let displayedBalancesLocalStorage = JSON.parse(window.localStorage.getItem("displayedBalances"));
-        if (displayedBalancesLocalStorage) {
-            setDisplayedBalances(displayedBalancesLocalStorage);
-        }
-    }
+    const [displayedBalances, setDisplayedBalances] = React.useState(JSON.parse(window.localStorage.getItem("displayedBalances")));
 
     const fetchProfileId = async () => {
-        console.log("fetching TW profile id")
+        console.log("Fetching TW profile ID...")
         const profileIdResponse = await fetch(apiUrl + `/v1/profiles`,
             {headers: {'Authorization': `Bearer ${token}`}});
         const profileArray = await profileIdResponse.json();
         const profileId = profileArray[0].id
         window.localStorage.setItem('transferwiseProfileId', profileId);
+        console.log(`TW profile ID fetched: ${profileId}`)
         return profileId;
     }
 
@@ -37,7 +31,7 @@ export default function BalanceContainer() {
             profileId = await fetchProfileId();
         }
 
-        console.log("fetching TW balance")
+        console.log("Fetching balance...")
         const balanceResponse = await fetch(apiUrl + `/v1/borderless-accounts?profileId=${profileId}`,
             {headers: {'Authorization': `Bearer ${token}`}});
         const balanceArray = await balanceResponse.json();
@@ -45,11 +39,12 @@ export default function BalanceContainer() {
         window.localStorage.setItem('balance', JSON.stringify(b));
         window.localStorage.setItem('balanceFetchTime', new Date().toString());
 
-        if (!displayedBalances) {
+        if (displayedBalances === null) {
             const allCurrencies = b.balances.map(b => b.currency)
             window.localStorage.setItem("displayedBalances", JSON.stringify(allCurrencies))
+            setDisplayedBalances(allCurrencies)
         }
-
+        console.log("Balance fetched successfully")
         setBalance(b);
     }
 
@@ -58,22 +53,24 @@ export default function BalanceContainer() {
         if (existingBalance !== null) {
             setBalance(existingBalance)
         } else {
-            console.log('Trying to reuse existing balance failed, balance not found.')
+            console.error('Trying to reuse existing balance failed, balance not found.')
         }
     }
 
-    const fetchAfterTime = new Date(new Date() - 5 * 60 * 1000); // five minutes ago
-    const balanceFetchTime = Date.parse(window.localStorage.getItem('balanceFetchTime'));
+    useEffect(() => {
+        const fetchAfterTime = new Date(new Date() - 5 * 60 * 1000); // five minutes ago
+        const balanceFetchTime = Date.parse(window.localStorage.getItem('balanceFetchTime'));
 
-    if (isNaN(balanceFetchTime) || fetchAfterTime > balanceFetchTime) {
-        fetchBalance()
-            .catch(e => {
-                console.log(e);
-                reuseExistingBalance();
-            });
-    } else if (Object.keys(balance).length === 1) { // balance has not been initialized yet
-        reuseExistingBalance();
-    }
+        if (isNaN(balanceFetchTime) || fetchAfterTime > balanceFetchTime) {
+            fetchBalance()
+                .catch(e => {
+                    console.log(e);
+                    reuseExistingBalance();
+                });
+        } else { // balance has not been initialized yet
+            reuseExistingBalance();
+        }
+    }, [])
 
     const refreshBalance = (event) => {
         const target = event.target;
@@ -84,10 +81,6 @@ export default function BalanceContainer() {
                 reuseExistingBalance();
             });
         setTimeout(() => target.classList.remove('spin-animation'), 1000);
-    }
-
-    const toggleSettingsModal = (event) => {
-        setModalOpen(!modalOpen);
     }
 
     const toggleBalanceDisplayed = (balance) => {
@@ -109,36 +102,36 @@ export default function BalanceContainer() {
     return (
         <div className={"balance-container"}>
             <div className={"balance"}>
-                <img src={process.env.PUBLIC_URL + "/transferwise.png"} alt={'TransferWise'} className={'transferwise-icon'}/>
+                <img src={process.env.PUBLIC_URL + "/transferwise.png"} alt={'TransferWise'}
+                     className={'transferwise-icon'}/>
                 <h2>
                     Current Balance
                     <button className={'balance-container-button'} onClick={refreshBalance}>
                         <img src={process.env.PUBLIC_URL + "/refresh.png"} alt={'refresh'} className={'refresh-icon'}/>
                     </button>
-                    <button className={'balance-container-button'} onClick={toggleSettingsModal}>
-                        <img src={process.env.PUBLIC_URL + "/settings.png"} alt={'settings'} className={'settings-icon'}/>
+                    <button className={'balance-container-button'} onClick={() => setModalOpen(!modalOpen)}>
+                        <img src={process.env.PUBLIC_URL + "/settings.png"} alt={'settings'}
+                             className={'settings-icon'}/>
                     </button>
                 </h2>
-                <Modal isOpen={modalOpen} style={{
+                <Modal isOpen={modalOpen} shouldCloseOnOverlayClick={true} shouldCloseOnEsc={true} onRequestClose={() => setModalOpen(false)} style={{
                     overlay: {
+                        backgroundColor: 'rgba(255, 255, 255, 0)',
+                    },
+                    content: {
                         position: 'fixed',
                         top: '49vh',
                         left: '16vw',
                         right: '73vw',
                         bottom: '20vh',
-                        borderRadius: '4px',
-                    },
-                    content: {
-                        position: 'absolute',
-                        top: '0',
-                        left: '0',
-                        right: '0',
-                        bottom: '0',
-                        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+                        borderRadius: '5px',
+                        backgroundColor: 'rgba(255, 255, 255, 0.7)',
                         padding: '20px'
                     }
-                }}>
-                    <SettingsModal balances={balance.balances} displayedBalances={displayedBalances} onCheckboxToggle={toggleBalanceDisplayed}/>
+                }}
+                >
+                    <SettingsModal balances={balance.balances} displayedBalances={displayedBalances}
+                                   onCheckboxToggle={toggleBalanceDisplayed}/>
                 </Modal>
                 {currencyBalances}
             </div>
