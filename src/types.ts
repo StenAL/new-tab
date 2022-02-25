9f545d9894025c7961d839c24a67017c9c5060d6
Sten Laane <laanesten@gmail.com>
2022-02-25 21:31:00 +0000

    Use newer versions of TransferWise API
    Both previously used endpoints were deprecated. The replacement for the
    /borderless-accounts endpoint now returns balances directly so renaming
    a bunch of variables and changing some types to support that.

--- src/types.ts
+++ src/types.ts
@@ -1,42 +1,26 @@
-export interface BankDetails {
-    id: number;
-    currency: string;
-    bankCode: string;
-}
 export interface Amount {
     value: number;
     currency: string;
 }
 
-export interface ProfileDetails {
+/**
+ * Returned by TransferWise API (GET /v2/profiles)
+ */
+export interface Profile {
+    id: number;
+    userId: number;
+    type: string;
     firstName: string;
     lastName: string;
 }
 
 /**
- * Returned by TransferWise API
+ * Returned by TransferWise API (GET /v3/profiles/{profile-id}/balances?types=STANDARD)
  */
-export interface Profile {
-    id: number;
-    type: string;
-    details: ProfileDetails;
-}
-
-/**
- * Returned by TransferWise API
- */
-export interface AccountInfo {
-    id: number;
-    profileId: number;
-    active: boolean;
-    balances: Balance[];
-}
-
 export interface Balance {
     id: number;
-    balanceType: string;
+    type: string;
     currency: string;
     amount: Amount;
     reservedAmount: Amount;
-    bankDetails: BankDetails;
 }
