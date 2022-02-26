30cfd011222c2238bc8644c1a693c7c7beb7a5fa
Sten Laane <laanesten@gmail.com>
2022-02-26 09:18:07 +0000

    Introduce reducer for state management
    No more callbacks!

--- src/components/BalanceContainer.tsx
+++ src/components/BalanceContainer.tsx
@@ -1,11 +1,18 @@
-import { FunctionComponent, useCallback, useEffect, useState, MouseEvent } from "react";
+import { FunctionComponent, useCallback, useEffect, useState, MouseEvent, useReducer, useMemo } from "react";
 import "currency-flags/dist/currency-flags.css";
+import { ActionType, DispatchContextProvider, reducer } from "../Reducer";
 import { Balance, Profile } from "../types";
 import { CurrencyBalance } from "./CurrencyBalance";
 import * as Modal from "react-modal";
 import { SettingsModal } from "./SettingsModal";
 
 Modal.setAppElement("#root");
+
+export interface AppState {
+    balances: Balance[];
+    isModalOpen: boolean;
+    displayedBalances: string[];
+}
 
 export const BalanceContainer: FunctionComponent = () => {
     const [token] = useState(process.env.REACT_APP_TRANSFERWISE_API_TOKEN);
@@ -16,11 +23,11 @@ export const BalanceContainer: FunctionComponent = () => {
     }
     const [apiUrl] = useState(process.env.REACT_APP_TRANSFERWISE_API_URL || "https://api.transferwise.com");
 
-    const [balances, setBalances] = useState<Balance[]>([]);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [displayedBalances, setDisplayedBalances] = useState<string[]>(
-        JSON.parse(localStorage.getItem("displayedBalances") || "[]")
-    );
+    const [state, dispatch] = useReducer(reducer, {
+        balances: [] as Balance[], // todo can use JSON.parse here?
+        isModalOpen: false,
+        displayedBalances: JSON.parse(localStorage.getItem("displayedBalances") || "[]"),
+    });
 
     const fetchProfileId = useCallback(async () => {
         console.log("Fetching TransferWise profile ID...");
@@ -29,46 +36,40 @@ export const BalanceContainer: FunctionComponent = () => {
         });
         const profileResponseArray: Profile[] = await profileResponse.json();
         const profileId = profileResponseArray[0].id;
-        window.localStorage.setItem("transferwiseProfileId", profileId.toString());
+        localStorage.setItem("transferwiseProfileId", profileId.toString());
         console.log(`TransferWise profile id fetched: ${profileId}`);
         return profileId;
     }, [apiUrl, token]);
 
     const fetchBalance = useCallback(async () => {
-        let profileId = window.localStorage.getItem("transferwiseProfileId") || undefined;
-        if (!profileId) {
-            profileId = (await fetchProfileId()).toString();
-        }
+        const fetchData = async () => {
+            let profileId = localStorage.getItem("transferwiseProfileId") || undefined;
+            if (!profileId) {
+                profileId = (await fetchProfileId()).toString();
+            }
 
-        console.log("Fetching balance...");
-        const response = await fetch(`${apiUrl}/v3/profiles/${profileId}/balances?types=STANDARD`, {
-            headers: { Authorization: `Bearer ${token}` },
-        });
-        const balancesResponse: Balance[] = await response.json();
-        window.localStorage.setItem("balances", JSON.stringify(balancesResponse));
-        window.localStorage.setItem("balanceFetchTime", new Date().getTime().toString());
-
-        if (displayedBalances.length === 0) {
-            const allCurrencies = balancesResponse.map((b) => b.currency);
-            window.localStorage.setItem("displayedBalances", JSON.stringify(allCurrencies));
-            setDisplayedBalances(allCurrencies);
-        }
-        console.log("Balance fetched successfully");
-        setBalances(balancesResponse);
-    }, [apiUrl, displayedBalances, fetchProfileId, token]);
+            console.log("Fetching balance...");
+            const response = await fetch(`${apiUrl}/v3/profiles/${profileId}/balances?types=STANDARD`, {
+                headers: { Authorization: `Bearer ${token}` },
+            });
+            const balances: Balance[] = await response.json();
+            dispatch({ type: ActionType.FETCH_DATA, balances: balances });
+        };
+        fetchData().catch((e) => console.error(`Error while fetching data: ${e}`));
+    }, [apiUrl, fetchProfileId, token]);
 
     const reuseExistingBalance = useCallback(() => {
         const existingBalance: Balance[] = JSON.parse(localStorage.getItem("balances") || "[]");
         if (Object.keys(existingBalance).length !== 0) {
-            setBalances(existingBalance);
+            dispatch({ type: ActionType.FETCH_DATA, balances: existingBalance });
         } else {
-            console.error("Trying to reuse existing balance failed but can not find any balances.");
+            console.error("Trying to reuse existing balance but can not find any balances.");
         }
     }, []);
 
     useEffect(() => {
         const fetchAfterTime = new Date().getTime() - 5 * 60 * 1000; // five minutes ago
-        const balanceFetchTime = Number(window.localStorage.getItem("balanceFetchTime"));
+        const balanceFetchTime = Number(localStorage.getItem("balanceFetchTime"));
 
         if (isNaN(balanceFetchTime) || fetchAfterTime > balanceFetchTime) {
             fetchBalance().catch((e) => {
@@ -93,80 +94,70 @@ export const BalanceContainer: FunctionComponent = () => {
         [fetchBalance, reuseExistingBalance]
     );
 
-    const toggleBalanceDisplayed = useCallback(
-        (currency: string) => {
-            let newBalances: string[];
-            if (displayedBalances.includes(currency)) {
-                newBalances = displayedBalances.filter((b) => b !== currency);
-            } else {
-                newBalances = displayedBalances.slice();
-                newBalances.push(currency);
-            }
-            localStorage.setItem("displayedBalances", JSON.stringify(newBalances));
-            setDisplayedBalances(newBalances);
-        },
-        [displayedBalances]
+    const currencyBalances = useMemo(
+        () =>
+            state.balances
+                .filter((b) => state.displayedBalances.includes(b.currency))
+                .map((b) => <CurrencyBalance currency={b.currency} value={b.amount.value} key={b.currency} />),
+        [state.balances, state.displayedBalances]
     );
 
-    const currencyBalances = balances
-        .filter((b) => displayedBalances.includes(b.currency))
-        .map((b) => <CurrencyBalance currency={b.currency} value={b.amount.value} key={b.currency} />);
-
     return (
-        <div className={"balance-container"}>
-            <div className={"balance"}>
-                <img
-                    src={process.env.PUBLIC_URL + "/transferwise.png"}
-                    alt={"TransferWise"}
-                    className={"transferwise-icon"}
-                />
-                <h2>
-                    Current Balance
-                    <button className={"balance-container-button"}>
-                        <img
-                            onClick={refreshBalance}
-                            src={process.env.PUBLIC_URL + "/refresh.png"}
-                            alt={"refresh"}
-                            className={"refresh-icon"}
-                        />
-                    </button>
-                    <button className={"balance-container-button"} onClick={() => setIsModalOpen(!isModalOpen)}>
-                        <img
-                            src={process.env.PUBLIC_URL + "/settings.png"}
-                            alt={"settings"}
-                            className={"settings-icon"}
-                        />
-                    </button>
-                </h2>
-                <Modal
-                    isOpen={isModalOpen}
-                    shouldCloseOnOverlayClick={true}
-                    shouldCloseOnEsc={true}
-                    onRequestClose={() => setIsModalOpen(false)}
-                    style={{
-                        overlay: {
-                            backgroundColor: "rgba(255, 255, 255, 0)",
-                        },
-                        content: {
-                            position: "fixed",
-                            top: "49vh",
-                            left: "16vw",
-                            right: "73vw",
-                            bottom: "20vh",
-                            borderRadius: "5px",
-                            backgroundColor: "rgba(255, 255, 255, 0.7)",
-                            padding: "20px",
-                        },
-                    }}
-                >
-                    <SettingsModal
-                        balances={balances}
-                        displayedBalances={displayedBalances}
-                        onCheckboxToggle={toggleBalanceDisplayed}
+        <DispatchContextProvider value={dispatch}>
+            <div className={"balance-container"}>
+                <div className={"balance"}>
+                    <img
+                        src={process.env.PUBLIC_URL + "/transferwise.png"}
+                        alt={"TransferWise"}
+                        className={"transferwise-icon"}
                     />
-                </Modal>
-                {currencyBalances}
+                    <h2>
+                        Current Balance
+                        <button className={"balance-container-button"}>
+                            <img
+                                onClick={refreshBalance}
+                                src={process.env.PUBLIC_URL + "/refresh.png"}
+                                alt={"refresh"}
+                                className={"refresh-icon"}
+                            />
+                        </button>
+                        <button
+                            className={"balance-container-button"}
+                            onClick={() => dispatch({ type: ActionType.TOGGLE_MODAL_OPEN })}
+                        >
+                            <img
+                                src={process.env.PUBLIC_URL + "/settings.png"}
+                                alt={"settings"}
+                                className={"settings-icon"}
+                            />
+                        </button>
+                    </h2>
+                    <Modal
+                        isOpen={state.isModalOpen}
+                        shouldCloseOnOverlayClick={true}
+                        shouldCloseOnEsc={true}
+                        onRequestClose={() => dispatch({ type: ActionType.CLOSE_MODAL })}
+                        style={{
+                            overlay: {
+                                backgroundColor: "rgba(255, 255, 255, 0)",
+                            },
+                            content: {
+                                position: "fixed",
+                                top: "49vh",
+                                left: "16vw",
+                                right: "73vw",
+                                bottom: "20vh",
+                                borderRadius: "5px",
+                                backgroundColor: "rgba(255, 255, 255, 0.7)",
+                                padding: "20px",
+                            },
+                        }}
+                    >
+                        <SettingsModal balances={state.balances} displayedBalances={state.displayedBalances} />
+                    </Modal>
+                    {currencyBalances}
+                </div>
             </div>
-        </div>
+        </DispatchContextProvider>
     );
 };
