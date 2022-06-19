c782200da9dad25c239ce7b3f76f282b7d4e15c7
Sten Laane <21343173+StenAL@users.noreply.github.com>
2022-06-19 12:56:11 +0100

    Upgrade to React 18
    Woo concurrent features! This required changing the way the root element
    is rendered to use 'createRoot' instead of 'render'.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
+
 import { App } from "./App";
 
 import "./index.scss";
 import "currency-flags/dist/currency-flags.css";
 
-render(
+const container = document.getElementById("root");
+if (!container) {
+    throw new Error("Failed to initialize React root, element with id 'root' is null");
+}
+
+const root = createRoot(container);
+root.render(
     <StrictMode>
         <App />
-    </StrictMode>,
-    document.getElementById("root")
+    </StrictMode>
 );
