32d3cda295934f955aba8cff91074ffd60042227
Sten Laane <21343173+StenAL@users.noreply.github.com>
2022-06-19 13:02:39 +0100

    Declare app root element to react-modal
    This fixes a warning from react-modal about accessibility issues,
    previously screen readers would not ignore the root app if the modal was
    open.
    
    More information: http://reactcommunity.org/react-modal/accessibility/

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import * as Modal from "react-modal";
 
 import { App } from "./App";
 
@@ -10,6 +11,7 @@ const container = document.getElementById("root");
 if (!container) {
     throw new Error("Failed to initialize React root, element with id 'root' is null");
 }
+Modal.setAppElement(container);
 
 const root = createRoot(container);
 root.render(
